e95a9ece052609aa14e089d9e03c2b2409f46dbd
vcevce <admin@venturecapital.enterprises>
2024-03-21 03:45:18 -0400

    add script to generate flat files; update flat  files

--- src/flat/AccountV3ModifiedUpgradable.sol
+++ src/flat/AccountV3ModifiedUpgradable.sol
@@ -1828,8 +1828,10 @@ error ContractCreationFailed();
 error NotAuthorized();
 error InvalidInput();
 error ExceedsMaxLockTime();
+error LockTimeInPast();
 error AccountLocked();
 error AccountUnlocked();
+error AccountTimeLocked();
 error InvalidAccountProof();
 error InvalidGuardian();
 error InvalidImplementation();
@@ -1841,21 +1843,61 @@ error OwnershipCycle();
 
 /**
  * @title Account Lock and Unlock
- * @dev Allows the root owner of a token bound account to lock access to an account until
- * unlocked by themselves or a future owner
+ * @dev Allows the root owner of a token bound account to lock access to an account
+ *
+ * A User can either lock their account until a certain time, or soft lock their account.
+ * The User may soft lock to lock in assets for a buy-it-now sale, or to prevent use of the account.
+ * The User may hard lock to lock in assets for all marketplace sales (bids, etc).
+ *
+ * @dev A soft lock is a lock that can be removed by the root owner at any time
+ * @dev A hard/time lock is a lock that can only be removed at its expiration, or on token transfer
  */
 abstract contract LockableUnlockable {
     /**
-     * @notice Boolean representing the lock status of the account
+     * @notice Boolean representing the soft lock status of the account
+     * A soft lock is a lock that can be removed by the root owner at any time
      */
-    bool public locked;
+    bool public softLocked;
 
-    event LockUpdated(bool locked);
+    /**
+     * @notice The timestamp at which this account will be unlocked
+     * A hard/time lock is a lock that can only be removed at expiration, or on token transfer
+     */
+    uint256 public lockedUntil;
+
+    event LockUpdated(bool softLocked, uint256 lockedUntil);
 
     /**
      * @dev Locks the account until a certain timestamp
+     *
+     * @param _lockedUntil The time at which this account will no longer be locked
      */
-    function lock() external virtual {
+    function lock(uint256 _lockedUntil) external virtual {
+        (uint256 chainId, address tokenContract, uint256 tokenId) = ERC6551AccountLib.token();
+        address _owner = _rootTokenOwner(chainId, tokenContract, tokenId);
+
+        if (_owner == address(0)) revert NotAuthorized();
+        if (msg.sender != _owner) revert NotAuthorized();
+
+        if (_lockedUntil > block.timestamp + 365 days) {
+            revert ExceedsMaxLockTime();
+        }
+
+        if (_lockedUntil < block.timestamp) {
+            revert LockTimeInPast();
+        }
+
+        _beforeLock();
+
+        lockedUntil = _lockedUntil;
+
+        emit LockUpdated(false, _lockedUntil);
+    }
+
+    /**
+     * @dev Locks the account until owner unlocks it
+     */
+    function softLock() external virtual {
         (uint256 chainId, address tokenContract, uint256 tokenId) = ERC6551AccountLib.token();
         address _owner = _rootTokenOwner(chainId, tokenContract, tokenId);
 
@@ -1864,15 +1906,18 @@ abstract contract LockableUnlockable {
 
         _beforeLock();
 
-        locked = true;
+        softLocked = true;
 
-        emit LockUpdated(true);
+        emit LockUpdated(true, lockedUntil);
     }
 
     /**
-     * @dev Unlocks the account
+     * @dev Unlocks the account when soft locked
      */
     function unlock() external virtual {
+        // If the account is hard locked, the lock must expire or token ownership must change
+        if (lockedUntil > block.timestamp) revert AccountTimeLocked();
+
         (uint256 chainId, address tokenContract, uint256 tokenId) = ERC6551AccountLib.token();
         address _owner = _rootTokenOwner(chainId, tokenContract, tokenId);
 
@@ -1881,16 +1926,52 @@ abstract contract LockableUnlockable {
 
         _beforeUnlock();
 
-        locked = false;
+        softLocked = false;
 
-        emit LockUpdated(false);
+        emit LockUpdated(false, lockedUntil);
+    }
+
+    /**
+     * @dev Called when the token associated with this account is transferred
+     * token contract is responsible for only calling this function for a token with an account
+     */
+    function onAccountBearingTokenTransfer(uint256 _tokenId) external virtual {
+        (, address tokenContract, uint256 tokenId) = ERC6551AccountLib.token();
+
+        // Must be called by the token contract for the correct token
+        if (_tokenId != tokenId) revert NotAuthorized();
+        if (msg.sender != tokenContract) revert NotAuthorized();
+
+        // If the account is hard locked, unlock it
+        if (lockedUntil > block.timestamp) {
+          lockedUntil = 0;
+        }
+
+        // If the account is soft locked, unlock it
+        if (softLocked) {
+          softLocked = false;
+        }
+
+        emit LockUpdated(false, 0);
     }
 
     /**
      * @dev Returns the current lock status of the account as a boolean
      */
     function isLocked() public view virtual returns (bool) {
-        return locked;
+        return softLocked || (lockedUntil > block.timestamp);
+    }
+
+    function isSoftLocked() public view virtual returns (bool) {
+        return softLocked;
+    }
+
+    function isHardLocked() public view virtual returns (bool) {
+        return lockedUntil > block.timestamp;
+    }
+
+    function lockInfo() public view virtual returns (bool, uint256) {
+        return (softLocked, lockedUntil);
     }
 
     function _rootTokenOwner(uint256 chainId, address tokenContract, uint256 tokenId)
---
+++ src/makeflat.sh
@@ -0,0 +1,6 @@
+rm -rf flat
+mkdir flat
+forge flatten -o flat/AccountGuardian.sol AccountGuardian.sol
+forge flatten -o flat/AccountProxy.sol AccountProxy.sol
+forge flatten -o flat/AccountV3Modified.sol AccountV3Modified.sol
+forge flatten -o flat/AccountV3ModifiedUpgradable.sol AccountV3ModifiedUpgradable.sol
